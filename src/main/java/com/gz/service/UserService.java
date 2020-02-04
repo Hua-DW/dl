efba910b963dd7ad2662a93460328ce655a22735
hdw_glb <hdw_glb@qq.com>
2020-02-04 15:52:18 +0800

    2020新增分支-分支后的主线

--- src/main/java/com/gz/service/UserService.java
+++ src/main/java/com/gz/service/UserService.java
@@ -15,7 +15,7 @@ public class UserService {
 
     public User getUser(String username){
 
-        System.out.println("1235621");
+        System.out.println("分支后的主线");
         return userMapper.getUser(username);
 
     }
